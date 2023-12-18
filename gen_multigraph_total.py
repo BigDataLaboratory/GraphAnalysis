efb724764b8ee52d3e8b1206c44da2041721344d
danielepasquini <psqdnl@hotmail.it>
2023-12-18 16:08:20 +0100

    Split the gen_multigraph method into different methods based on the relationship type

--- gen_multigraph_total.py
+++ gen_multigraph_total.py
@@ -32,37 +32,47 @@ class Multigraph:
             hashed.sort()
             return list(combinations(hashed, 2))
 
-    def gen_multigraph_total(self, tweets):
+    def gen_multigraph(self, df_list):
+        """
+        Give a list of dataframes that represent different edgelists, build a single dataframe concat each dataframe
+        in the input list
+        :param df_list: list of dataframes that represent different edgelists
+        :return: single dataframe concat each dataframe in the input list
+        """
+        # Concatenazione dei DataFrame
+        result_df = pd.concat(df_list, ignore_index=True)
+        return result_df
 
+    def relationship_retweet(self, tweets):
         # DataFrame e_rt (retweet)
         e_rt_src = tweets['user.id']
         e_rt_dst = tweets['retweeted_status.user.id']
         e_rt = pd.DataFrame({'src': e_rt_src.apply(self.hash), 'dst': e_rt_dst.apply(self.hash)})
-        e_rt['weight'] = 1
         e_rt['relationship'] = 'retweet'
 
+    def relationship_hashtag(self, tweets):
         # DataFrame e_ht (hashtag)
         e_ht_src = tweets['user.id']
-        e_ht_dst = tweets['hashtagEntities'].apply(lambda x: x.lower().split('|') if isinstance(x, str) else [])    # Applica una funzione anonima che fa lo split se il valore è una stringa 
+        e_ht_dst = tweets['hashtagEntities'].apply(lambda x: x.lower().split('|') if isinstance(x, str) else [])    # Applica una funzione anonima che fa lo split se il valore è una stringa
         e_ht = pd.DataFrame({'src': e_ht_src, 'dst': e_ht_dst})
         e_ht = e_ht.explode('dst').dropna()     # Rimozione NaN, altrimenti TypeError dato che vengono considerati come Float
         e_ht['dst'] = e_ht['dst'].apply(self.compute_hash)
-        e_ht['weight'] = 1
         e_ht['relationship'] = 'hashtag'
 
+    def relationship_cooccurences(self, tweets):
         # DataFrame e_ht_ht (cooccurrences)
         ht_ht_src = tweets['user.id']
-        ht_ht_dst = tweets['hashtagEntities'].apply(lambda x: self.combinations_list(x.lower().split('|')) if isinstance(x, str) else [])   # Applica una funzione anonima che fa lo split se il valore è una stringa 
+        ht_ht_dst = tweets['hashtagEntities'].apply(lambda x: self.combinations_list(x.lower().split('|')) if isinstance(x, str) else [])   # Applica una funzione anonima che fa lo split se il valore è una stringa
         e_ht_ht = pd.DataFrame({'src': ht_ht_src, 'dst': ht_ht_dst})
         e_ht_ht = e_ht_ht.explode('dst').dropna()
-        e_ht_ht['weight'] = 1
         e_ht_ht['relationship'] = 'cooccurrences'
 
-        # Concatenazione dei DataFrame
-        result_df = pd.concat([e_rt, e_ht, e_ht_ht], ignore_index=True)
-    
-        return result_df
-        
+    def relationship_responses(self, tweets):
+        pass
+
+    def relationship_mentions(self, tweets):
+        pass
+
 
 #pd.set_option('display.max_columns', None)
 #pd.set_option('display.max_colwidth', 50)
