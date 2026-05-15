9e19bdb43e8e259ab4e8cb3881da29f15eee362d
Nasser kotiyev <kotinass2003@gmail.com>
2026-05-15 20:05:41 +0200

    changing batch size to 20

--- algorithms/GraphGenerationUser.py
+++ algorithms/GraphGenerationUser.py
@@ -308,6 +308,7 @@ class CommunityAwareShardStrategy(ExtractionStrategy):
         for cid, user_ids in work_item:
             for i in range(0, len(user_ids), self.batch_size):
                 batch = user_ids[i : i + self.batch_size]
+                print("[CommunityAwareShardStrategy] Query:{'user.id': {'$in': "+batch+"}}")
                 cursor = col.find(
                     {"user.id": {"$in": batch}}, proj
                 ).sort("user.id", 1)
